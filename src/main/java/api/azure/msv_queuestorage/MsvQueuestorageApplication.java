9158d69d01ef00bf59b1c7770a21ed1d8fd4b3b7
Joseph Magallanes <josephcarlos.jcmn@gmail.com>
2024-09-27 00:33:56 -0500

    first commit

---
+++ src/main/java/api/azure/msv_queuestorage/MsvQueuestorageApplication.java
@@ -0,0 +1,13 @@
+package api.azure.msv_queuestorage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsvQueuestorageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsvQueuestorageApplication.class, args);
+	}
+
+}
---
+++ src/test/java/api/azure/msv_queuestorage/MsvQueuestorageApplicationTests.java
@@ -0,0 +1,13 @@
+package api.azure.msv_queuestorage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsvQueuestorageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
